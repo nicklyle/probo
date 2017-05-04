3cdf1b6654c1b9ab783ab3245478497424782c4d
nicklyle <nicholas.lyle@aggiemail.usu.edu>
2017-05-04 13:00:48 -0600

    Adding strange payoff class

--- probo/payoff.py
+++ probo/payoff.py
@@ -57,5 +57,36 @@ def asian_call_payoff(option, spot)
 def asian_put_payoff(option, spot)
     return maximum(option.strike - np.mean(spot),0.0) 
 
+class StrangePayoff(Payoff):
+    def __init__(self, expiry, strike, payoff):
+        self.__expiry = expiry
+        self.__strike = strike
+        self.__payoff = payoff
+        
+    @property
+    def expiry(self):
+        return self.__expiry
+
+    @expiry.setter
+    def expiry(self, new_expiry):
+        self.__expiry = new_expiry
+    
+    @property 
+    def strike(self):
+        return self.__strike
+    
+    @strike.setter
+    def strike(self, new_strike):
+        self.__strike = new_strike
+
+    def squarepayoff(self, spot):
+        return self.__payoff(self, spot*spot)
+    
+    def rootpayoff(self, spot):
+        return self.__payoff(self, np.sqrt(spot))
+    
+    def fracpayoff(self, spot_:
+        return self.__payoff(self, 1/spot)
+
 
 
